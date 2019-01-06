91551cdf2480870c12daf9fd47f3161f58cb71b7
chancity <hawbakerchancey@gmail.com>
2019-01-05 22:10:20 -0800

    new branch

--- Toss.Server/Services/CaptchaMediatRAdapter.cs
+++ Toss.Server/Services/CaptchaMediatRAdapter.cs
@@ -1,28 +1,28 @@
-using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
 using Toss.Shared;
 
 namespace Toss.Server.Services
 {
-
     public class CaptchaMediatRAdapter<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
-        private ICaptchaValidator captchaValidator;
+        private readonly ICaptchaValidator captchaValidator;
 
         public CaptchaMediatRAdapter(ICaptchaValidator captchaValidator)
         {
             this.captchaValidator = captchaValidator;
         }
-        
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
         {
             if (request is NotARobot notARobot)
             {
-                await this.captchaValidator.Check(notARobot.Token);
+                await captchaValidator.Check(notARobot.Token);
             }
-           
+
             return await next();
         }
     }
